ad3b23647246e8f19c60709f724a9bbb45ec1aef
akashkahalkar <sky94308@gmail.com>
2016-11-27 19:52:35 +0530

    no new updates

--- All_run_shortcuts_reg_query.bat
+++ All_run_shortcuts_reg_query.bat
@@ -1,5 +1,5 @@
 @echo off
-echo ^<----List Of Run Shortcut-----^> > shrtcut_temp.txt
+echo ^<---- List Of Run Shortcut----^> > shrtcut_temp.txt
 for /f "tokens=7 delims=\" %%I IN ('reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths"') DO echo %%I >> shrtcut_temp.txt
 start notepad shrtcut_temp.txt
 exit
